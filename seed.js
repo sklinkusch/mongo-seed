ffd4fa5de3b844697002b715e367c25eaea6ba85
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-05 11:44:26 +0200

    modify text to user

--- seed.js
+++ seed.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/seed-test', { useNewUrlParser: true });
 
-const Cat = mongoose.model('Cat', { name: String });
+const User = mongoose.model('User', { name: String, username: String, email: String, age: Number });
 
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
+const user1 = new User({ name: 'Stefan Klinkusch', username: "sklinkusch", email: "stefan.klinkusch@googlemail.com" });
+
+const user2 = new User({ name: 'Kyle Simpson', username: "kylesim", email: "kyle@gmail.com" });
+
+user1.save().then(() => console.log('Meow!'));
+user2.save().then(() => console.log('Woohoo!'));
