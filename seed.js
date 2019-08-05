57d109d9f45d2849fd61b6cc35b0607b6d6e92e2
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-05 13:44:00 +0200

    further checking

--- seed.js
+++ seed.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/seed-test', { useNewUrlParser: true });
 
-const User = mongoose.model('User', { name: String, username: String, email: String, age: Number });
+const userSchema = new mongoose.Schema({
+  name: String,
+  username: { type: String, minlength: 4, maxlength: 255 },
+  email: String,
+  age: Number
+})
 
-const user1 = new User({ name: 'Stefan Klinkusch', username: "sklinkusch", email: "stefan.klinkusch@googlemail.com" });
+const User = mongoose.model('User', userSchema);
 
-const user2 = new User({ name: 'Kyle Simpson', username: "kylesim", email: "kyle@gmail.com" });
+// const user1 = new User({ name: 'Stefan Klinkusch', username: "sklinkusch", email: "stefan.klinkusch@googlemail.com" });
 
-user1.save().then(() => console.log('Meow!'));
-user2.save().then(() => console.log('Woohoo!'));
+// const user2 = new User({ name: 'Kyle Simpson', username: "kylesim", email: "kyle@gmail.com" });
+
+const user3 = new User({ name: "Dummy", username: "me", email: "something@gmail.com" });
+
+// user1.save().then(() => console.log('Meow!'));
+// user2.save().then(() => console.log('Woohoo!'));
+user3.save().then(() => console.log('Yeah'));
